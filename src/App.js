0ee101ff7f009d70799f15f2c71d23ab982ae462
breznovic <slymarkel@gmail.com>
2021-11-10 12:10:04 +0300

    Beginning

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import './Styles/App.css'
-import { BrowserRouter } from 'react-router-dom'
-import Navbar from './Components/UI/Navbar/Navbar'
-import AppRouter from './Components/UI/AppRouter/AppRouter'
-import { AuthContext } from './context/context'
+import React from 'react'
+import './App.css'
+
 
 function App() {
-  const [isAuth, setIsAuth] = useState(false)
-  const [isLoading, setLoading] = useState(true)
-  useEffect(() => {
-    if (localStorage.getItem('auth')) {
-      setIsAuth(true)
-    }
-    setLoading(false)
-  }, [])
-  return (
-    <AuthContext.Provider value={{
-      isAuth,
-      setIsAuth,
-      isLoading
-    }}>
-      <BrowserRouter>
-        <Navbar />
-        <AppRouter />
-      </BrowserRouter>
-    </AuthContext.Provider>
-  )
+
+    return
+
 }
 
 export default App
--- src/Components/UI/Input/Input.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import classes from './Input.modules.css'
-
-const Input = React.forwardRef((props, ref) => {
-    return <input className={classes.myInput} ref={ref} {...props} /> 
-})
-
-export default Input
