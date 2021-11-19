7da28dd24cdd7d3223dd3b8209d90be0186dde4d
breznovic <slymarkel@gmail.com>
2021-11-19 10:06:20 +0300

    Another lesson

---
+++ src/04/04.test.ts
@@ -0,0 +1,48 @@
+import {CourseType} from "./04";
+
+test('should take old man older than 90', () => {
+    const ages = [18, 20, 22, 1, 100, 90, 14]
+
+    const oldAges = ages.filter(age => age > 90)
+
+    expect(oldAges.length).toBe(1)
+    expect(oldAges[0]).toBe(100)
+})
+
+test('should take courses cheaper 160', () => {
+    const courses = [
+        {
+            title: 'CSS',
+            price: 110
+        },
+        {
+            title: 'JS',
+            price: 200
+        },
+        {
+            title: 'React',
+            price: 150
+        }
+    ]
+
+    const cheapCourses = courses.filter(courses => courses.price < 160)
+
+    expect(cheapCourses.length).toBe(2)
+    expect(cheapCourses[0].title).toBe("CSS")
+    expect(cheapCourses[1].title).toBe("React")
+})
+
+test('get only completed tasks', () => {
+    const tasks = [
+        {id: 1, title: 'Bread', isDone: false},
+        {id: 2, title: 'Milk', isDone: true},
+        {id: 3, title: 'Salt', isDone: false},
+        {id: 4, title: 'Sugar', isDone: true},
+    ]
+
+    const uncompletedTasks = tasks.filter(task => !task.isDone)
+
+    expect(uncompletedTasks.length).toBe(2)
+    expect(uncompletedTasks[0].id).toBe(2)
+    expect(uncompletedTasks[1].title).toBe(4)
+})
